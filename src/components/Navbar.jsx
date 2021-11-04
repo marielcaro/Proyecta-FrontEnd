fce85c244d5e50a59d3bc93285999a8ed1a024e8
SebaSavino <sebastian.savino2020@gmail.com>
2021-11-03 22:03:39 -0300

    added bootstrap/first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => (
+    <ul className="nav">
+        <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link disabled">Disabled</a>
+        </li>
+    </ul>
+)
+
+export default Navbar
