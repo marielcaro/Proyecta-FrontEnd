c87ce419f344273f45b403eae766f58e85dd2399
AbelX18 <abelx8cam@gmail.com>
2021-11-26 00:47:45 -0300

    NavBar of the PostLogin created

--- src/Modules/Shared/components/Navbar/Navbar.jsx
+++ src/Modules/Shared/components/Navbar/Navbar.jsx
@@ -1,46 +1,46 @@
-import Logo from '../../../../assets/icons/layers.png'
-import "./navbar.css"
+import Logo from "../../../../assets/icons/layers.png";
+import Bell from "../../../../assets/icons/bell 1.svg";
+import Raton from "../../../../assets/icons/email.svg";
+import Menu from "../../../../assets/icons/ToggleMenu.svg";
+import "./navbar.css";
 
 export const Navbar = ({ isAuthenticated = false }) => {
+  const classNav = isAuthenticated
+    ? "custom-nav-post-login"
+    : "custom-nav-login"; // ternario
+  return (
+    <div
+      className={`row justify-content-between align-items-center ${classNav} nav-container`}
+    >
+        
+      <div className="col d-flex justify-content-start align-items-center">
+        <img className="mx-4"src={Menu}/>
+        <img
+          className="d-inline-block align-text-top text-white mx-2 proyecta-icon"
+          height="64"
+          src={Logo}
+          width="64"
+          alt="logo"
+        />
+        <p className="nav-title">Proyecta</p>
+      </div>
 
-    const classNav = isAuthenticated ? "custom-nav-post-login" :  "custom-nav-login" // ternario
-    return (
-        // template string | Javascript
-        <nav className={`navbar navbar-expand-lg navbar-light ${classNav} mb-3`}>
-            <div className="container-fluid nav-container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-                    <div className="nav-brand d-flex justify-content-center align-items-center">
-                        <img
-                            className="d-inline-block align-text-top text-white mx-2"
-                            height="64"
-                            src={Logo}
-                            width="64"
-                            alt="logo"
-                        />
-                        <p className="nav-title">Proyecta</p>
-                    </div>
-                    {/* <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a class="nav-link" href="#">Proyectos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a class="nav-link" href="#">¿Quienes somos?</a>
-                        </li>
-                        <li className="nav-item">
-                            <a class="nav-link btn btn-acceder">Acceder</a>
-                        </li>
-                    </ul> */}
-                </div>
-            </div>
-        </nav>
-    )
+      <div className="col d-flex justify-content-end align-items-center">
+        <button
+          type="submit"
+          className="btn btn-login rounded-pill btn-size-login  "
+        >
+          Quiero ser Líder de Proyecto
+        </button>
+
+        <img className="mx-4" src={Bell} height="32" width="32" />
+
+        <img className="img-fluid rounded-circle me-3" src={Raton} height="64" width="64" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
 
 
-}
-export default Navbar
