340e4a0b44e10d2440a86b763abc82fabf34eaad
LordGh0st <pablourojas@protonmail.com>
2021-11-30 02:03:55 -0300

    Card terminado

--- src/Modules/Admin/components/Sidebar/Sidebar.jsx
+++ src/Modules/Admin/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import style from "./sidebar.module.css";
 import { items } from './items'
 
@@ -5,7 +6,7 @@ import { ListItem } from './ListItem'
 
 export const Sidebar = () => {
   // Aqui nos vamos a traer la ruta actual en la que esta el usuario, si esta en www.pagina.com/reportes entonces mostramos REPORTES
-  const selected = "/tablero";
+  const [selected,setSelected] = useState("/tablero");
   return (
     <div className={style.cajablanca}>
       <div className="col tipografia">
@@ -15,6 +16,7 @@ export const Sidebar = () => {
               item={item}
               key={index}
               selected={selected === item.path}
+              setSelected={setSelected}
             />
           ))}
         </ul>
--- src/Modules/Admin/pages/Tablero/Tablero.jsx
+++ src/Modules/Admin/pages/Tablero/Tablero.jsx
@@ -4,6 +4,8 @@ import { BaseLayout } from '../../Layouts'
 export const Tablero = () => {
     return <BaseLayout>
         <ProyectCard />
+        <ProyectCard />
+        <ProyectCard />
     </BaseLayout>
 }
 
